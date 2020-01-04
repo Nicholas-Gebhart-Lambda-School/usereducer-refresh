256b54395653b2747000699b028959c7048a29be
gebhartn <nicholas.gebhart@gmail.com>
2020-01-04 06:42:28 -0800

    feat: adds urql client to root

--- src/index.js
+++ src/index.js
@@ -2,4 +2,19 @@ import React from "react";
 import { render } from "react-dom";
 import App from "./components/App";
 
-render(<App />, document.getElementById("root"));
+import { Provider, Client, dedupExchange, fetchExchange } from "urql";
+import { cacheExchange } from "@urql/graphql-graphcache";
+
+const cache = cacheExchange({});
+
+const client = new Client({
+  url: "http://localhost:4000",
+  exchanges: [dedupExchange, cache, fetchExchange]
+});
+
+render(
+  <Provider value={client}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
